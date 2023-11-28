b07759af7b08f7c6827c27a82bdc4db607b81a57
jiaweiw02 <wuj22@rpi.edu>
2023-11-28 15:31:16 -0500

    part2 COMP

--- hw3/prolog-pa3/part2.pl
+++ hw3/prolog-pa3/part2.pl
@@ -5,30 +5,37 @@
 %   for NLP parser make sure you match the type of the atom: 
 %      it may come in handy that print and write are two different method of printing within prolog 
 
-nlpParser([]).
-nlpParser([ [Person, has, item, Item, that, costs, Cost, _, _, _, _, Size, _ | _] | T]) :-
-    % atom(Person),
-    % atom(Item),
-    % number(Cost),
-    % number(Size),
-    writeln([Person, Item, Cost, Size]),
-    nlpParser(T).
+% item
+nlpParser([], Item, Item, Constraint, Constraint).
+nlpParser([[Person, has, item, Item, that, costs, Cost, _, _, _, _, Size, _ | _] | T], ItemC, ItemR, ConstraintC, ConstraintR) :-
+    atom_number(Cost, CostNumber),
+    atom_number(Size, SizeNumber),
+    nlpParser(T, [ [Person, Item, CostNumber, SizeNumber] | ItemC], ItemR, ConstraintC, ConstraintR).
 
-nlpParser([ [House, house, wants, total, price, CompPrice, than, Price, dollars, and, total, volume, CompSize, than, Size | _] | T]) :-
-    writeln([House, CompPrice, Price, CompSize, Size]),
-    nlpParser(T).
+% constraint
+nlpParser([[House, house, wants, total, price, CompPrice, than, Price, dollars, and, total, volume, CompSize, than, Size | _] | T],
+            ItemC, ItemR, ConstraintC, ConstraintR) :-
+    atom_number(Price, PriceNumber),
+    atom_number(Size, SizeNumber),
+    nlpParser(T, ItemC, ItemR, [ [House, CompPrice, PriceNumber, CompSize, SizeNumber] | ConstraintC], ConstraintR).
 
-nlpParser([ [House, house, wants, total, volume, CompSize, than, Size, cubic, feet, and, total, price, CompPrice, than, Price | _] | T]) :-
-    writeln([House, CompPrice, Price, CompSize, Size]),
-    nlpParser(T).
+% constraint
+nlpParser([[House, house, wants, total, volume, CompSize, than, Size, cubic, feet, and, total, price, CompPrice, than, Price | _] | T],
+            ItemC, ItemR, ConstraintC, ConstraintR) :-
+    atom_number(Price, PriceNumber),
+    atom_number(Size, SizeNumber),
+    nlpParser(T, ItemC, ItemR, [ [House, CompPrice, PriceNumber, CompSize, SizeNumber] | ConstraintC], ConstraintR).
 
 % Main 
 main :-
     current_prolog_flag(argv, [DataFile|_]),
     open(DataFile, read, Stream),
     read_file(Stream,Lines), %Lines contain all the information within line split by spaces, comma and period.
-    nlpParser(Lines),
-    % write(Lines),
+    nlpParser(Lines, [], ItemsReversed, [], ConstraintsReversed),
+    reverse(ItemsReversed, Items),
+    reverse(ConstraintsReversed, Constraints),
+    writeln(Items),
+    writeln(Constraints),
     close(Stream).
 
 
